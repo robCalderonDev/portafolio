8a12bfc7a5661d488da447aa1983106c0d4223e7
robert calderon <rob.calderonv@duocuc.cl>
2023-12-08 18:24:39 -0300

    fixed width about me

--- components/Header.jsx
+++ components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
                 <h1 className='text-3xl font-semibold '>Robert Calderon</h1>
                 <hr className="w-44 h-px my-1 bg-[#3B94A8]  border-0 " />
                 <h1 className='text-[#3B94A8]  text-2xl'>Web Developer</h1>
-                <div className='w-2/4 text-xl mt-10'>
+                <div className=' w-11/12 lg:w-2/4 text-center text-xl mt-10'>
                     <p className=' mb-10'>I am a passionate <span className='text-[#3B94A8] '> front-end developer</span>  with 1 year of experience deeply enthusiastic about  <span className='text-[#3B94A8]'>technology </span> and programming. I'm 23 years old and live in Chile. My primary focus is on web development and cloud technologies, two constantly evolving areas that inspire me to learn and grow continuously.
                     </p>
 
